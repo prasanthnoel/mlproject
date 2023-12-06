89c841f33f9c628e2e5709054ef03fc7e92031bb
Prasanth Noel <prasanthnoel3694@gmail.com>
2023-12-06 21:55:03 +0530

    flask app deployment is completed

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import os 
 import sys
 import dill
+import pickle 
 
 import numpy as np
 import pandas as pd
@@ -52,5 +53,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,params):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
